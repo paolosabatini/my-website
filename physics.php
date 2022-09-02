50ce728932c3200c1a0ae643906121d85bc8785b
Paolo <paolo.sabatini@cern.ch>
2022-09-02 22:16:21 +0200

    changed .html links to .php

--- physics.php
+++ physics.php
@@ -27,15 +27,15 @@ require ("init.php");
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="commNav">
       <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="index.html#page-top">home</a>
+        <a class="navbar-brand" href="index.php#page-top">home</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fas fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ms-auto">
-            <li class="nav-item"><a class="nav-link" href="index.html##about">about</a></li>
-            <li class="nav-item"><a class="nav-link" href="index.html##projects">projects</a></li>
+            <li class="nav-item"><a class="nav-link" href="index.php##about">about</a></li>
+            <li class="nav-item"><a class="nav-link" href="index.php##projects">projects</a></li>
             <li class="nav-item"><a class="nav-link" href="#contact">contact</a></li>
           </ul>
         </div>
